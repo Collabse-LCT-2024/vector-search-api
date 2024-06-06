680180872459575f77a139a0558b141c66b5855b
Daniel Khromov <danielkhromov@gmail.com>
2024-06-06 14:46:44 +0300

    multiple search modes, remove models

--- app/db.py
+++ app/db.py
@@ -1,13 +1,13 @@
+from typing import List
 import weaviate.classes as wvc
 from weaviate.classes.query import Filter
-from weaviate.classes.config import Property, DataType
+from weaviate.classes.config import Property
 import weaviate
-from weaviate.classes.query import MetadataQuery, GroupBy
+from weaviate.classes.query import GroupBy
 
 
 class DataBaseClient:
-    def __init__(self, WEAVIATE_HOST: str, index_name: str):
-        self.index_name = index_name
+    def __init__(self, WEAVIATE_HOST):
         self.weaviate_client = weaviate.connect_to_custom(
             http_host=WEAVIATE_HOST,
             http_port=8080,
@@ -16,11 +16,29 @@ class DataBaseClient:
             grpc_port=50051,
             grpc_secure=False,
         )
-        self.collection = self.weaviate_client.collections.get(self.index_name)
 
-    def search(self, query_vector, top_k):
+    def save_batch_embeddings(self, index_name, embeddings, properties: List[dict]):
+        self.collection = self.weaviate_client.collections.get(index_name)
+        question_objs = []
+        for i, item in enumerate(embeddings):
+            question_objs.append(
+                wvc.data.DataObject(
+                    properties=properties[i],
+                    vector=item,
+                )
+            )
+        self.collection.data.insert_many(question_objs)
+        print("saved batch")
+
+    def save_embedding(self, index_name, embedding, properties: dict):
+        self.collection = self.weaviate_client.collections.get(index_name)
+        self.collection.data.insert(properties=properties, vector=embedding)
+        print("saved embedding")
+
+    def search_group_by(self, index_name, query_vector, top_k, group_property):
+        self.collection = self.weaviate_client.collections.get(index_name)
         group_by = GroupBy(
-            prop="external_id",  # group by this property
+            prop=group_property,  # group by this property
             objects_per_group=1800,
             number_of_groups=top_k,  # maximum number of groups
         )
@@ -30,30 +48,40 @@ class DataBaseClient:
             limit=1000,
             return_metadata=wvc.query.MetadataQuery(certainty=True),
         )
+        print("SEARCHING")
+        return response
+
+    def search(self, index_name, query_vector, top_k):
+        self.collection = self.weaviate_client.collections.get(index_name)
+        response = self.collection.query.near_vector(
+            near_vector=query_vector,
+            limit=top_k,
+            return_metadata=wvc.query.MetadataQuery(certainty=True),
+        )
         return response
 
     def drop_collection(self, index_name):
         self.weaviate_client.collections.delete(index_name)
 
-    def create_collection(self, index_name: str):
+    def create_collection(self, name: str, properties: List[Property]):
         self.weaviate_client.collections.create(
-            index_name,
+            name,
             vectorizer_config=wvc.config.Configure.Vectorizer.none(),
             vector_index_config=wvc.config.Configure.VectorIndex.hnsw(
                 distance_metric=wvc.config.VectorDistances.COSINE
             ),
-            properties=[
-                Property(name="external_id", data_type=DataType.TEXT),
-                Property(name="link", data_type=DataType.TEXT),
-            ],
+            properties=properties,
         )
 
-    def fetch_all(self, include_vector: bool = False):
+    def fetch_all(self, index_name, include_vector: bool = False):
+        self.collection = self.weaviate_client.collections.get(index_name)
         for item in self.collection.iterator(include_vector=include_vector):
             print(item)
 
-    def is_present_by_id(self, unified_id: str):
+    def is_present_by_id(self, index_name, equal_to, by_property: str):
+        self.collection = self.weaviate_client.collections.get(index_name)
         response = self.collection.query.fetch_objects(
-            filters=Filter.by_property("external_id").equal(unified_id), limit=1
+            filters=Filter.by_property(by_property).equal(equal_to), limit=1000
         )
+        print(response.objects)
         return len(response.objects) > 0
--- app/main.py
+++ app/main.py
@@ -3,18 +3,23 @@ from db import DataBaseClient
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from clip_client import Client
-from models import TextEmbedding, Vector
 import translators as ts
+from pymongo import MongoClient
 
 load_dotenv(override=True)
 WEAVIATE_HOST = os.environ.get("WEAVIATE_HOST")
 CLIP_URL = os.environ.get("CLIP_URL")
+MONOGO_URI = os.environ.get("MONGO_URI")
 
 app = FastAPI(root_path="/api/v1")
 print("PREACCELERATING TRANSLATION. MAY TAKE UP TO 5 MINUTES")
 _ = ts.preaccelerate_and_speedtest()  # speed up translation.
-client = DataBaseClient(WEAVIATE_HOST, "EmbeddingsIndex")
+client = DataBaseClient(WEAVIATE_HOST)
+mongo = MongoClient(MONOGO_URI)
 clip_client = Client(CLIP_URL)
+db = mongo["storage"]
+collection = db["videos"]
+print("READY")
 
 
 @app.get("/version")
@@ -22,32 +27,8 @@ def read_root():
     return {"version": "0.1"}
 
 
-@app.post("/search/vector")
-def search_by_vector(
-    query_vector: list[float], top_k: int = 3, include_vector: bool = False
-) -> list[Vector]:
-    response = client.search(query_vector, top_k)
-    result = []
-    for o in response.objects:
-        if not include_vector:
-            result.append(
-                Vector(
-                    external_id=o.properties["external_id"],
-                    link=o.properties["link"],
-                )
-            )
-        else:
-            result.append(
-                Vector(
-                    external_id=o.properties["external_id"],
-                    link=o.properties["link"],
-                )
-            )
-    return result
-
-
 @app.post("/search/text")
-def search_by_text(text: str, top_k: int = 3) -> list[Vector]:
+def search_by_text(text: str, mode: int, top_k: int = 3):
     eng_text = ts.translate_text(
         query_text=text,
         translator="yandex",
@@ -55,23 +36,43 @@ def search_by_text(text: str, top_k: int = 3) -> list[Vector]:
         to_language="en",
         if_use_preacceleration=True,
     )
-    text_embedding = clip_client.encode([eng_text])[0]
-    response = client.search(text_embedding.tolist(), top_k)
-    result = []
-    for k, v in response.groups.items():  # View by group
-        result.append(
-            Vector(
-                external_id=v.objects[0].properties["external_id"],
-                link=v.objects[0].properties["link"],
-                similar_frame_count=v.number_of_objects,
-                max_distance=v.max_distance,
-            )
+    eng_text = text
+
+    query_vector = clip_client.encode([eng_text])[0].tolist()
+    if mode == 0:
+        response = client.search_group_by(
+            "FrameEmbeddings", query_vector, top_k, "external_id"
         )
+    if mode == 1:
+        response = client.search("MeanFrameEmbeddings", query_vector, top_k)
+    if mode == 2:
+        response = client.search("NormallyWeightedFrameEmbeddings", query_vector, top_k)
+
+    result = []
+    if mode == 0:
+        for k, group in response.groups.items():
+            print(group)
+            print()
+            external_id = group.objects[0].properties["external_id"]
+            relevant_frames_count = group.number_of_objects
+            link = collection.find_one({"uuid": external_id})["link"]
+            result.append(
+                {
+                    "external_id": external_id,
+                    "link": link,
+                    "relevant_frames_count": relevant_frames_count,
+                }
+            )
+    else:
+        for obj in response.objects:
+            external_id = obj.properties["external_id"]
+            link = collection.find_one({"uuid": external_id})["link"]
+            result.append({"external_id": external_id, "link": link})
     return result
 
 
 @app.get("/encode")
-def encode_text(text: str) -> TextEmbedding:
+def encode_text(text: str):
     eng_text = ts.translate_text(
         query_text=text,
         translator="yandex",
@@ -80,4 +81,4 @@ def encode_text(text: str) -> TextEmbedding:
         if_use_preacceleration=True,
     )
     text_embedding = clip_client.encode([eng_text])[0]
-    return TextEmbedding(vector=text_embedding)
+    return {"vector": text_embedding}
